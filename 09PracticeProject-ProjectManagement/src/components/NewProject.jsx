dc7db14866499262be402e6bed1fb12c4952b606
fanzejie <fanzejie@cslc.com.cn>
2024-02-15 21:51:29 +0800

    通过完成错误提醒弹窗复习了Portal,useImperativeHandle

--- 09PracticeProject-ProjectManagement/src/components/NewProject.jsx
+++ 09PracticeProject-ProjectManagement/src/components/NewProject.jsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Model";
 export default function NewProject({ onAdd }) {
+    const modal = useRef()
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,6 +13,16 @@ export default function NewProject({ onAdd }) {
     const enteredDueDate = dueDate.current.value;
 
     //然后这里要把这些数据传到app.jsx中管理，要用到状态提升
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate === ""
+    ) {
+      //展示错误界面
+      modal.current.open()
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -18,26 +30,36 @@ export default function NewProject({ onAdd }) {
     });
   }
   return (
-    <div className="w-[35rem]">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-6 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title"></Input>
-        <Input ref={description} label="Description" isTestArea={true}></Input>
-        <Input type="date" ref={dueDate} label="Due Date"></Input>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>Invalid Input</h2>
+        <p>you forgot to enter a value.</p>
+        <p>please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem]">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-6 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}>
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title"></Input>
+          <Input
+            ref={description}
+            label="Description"
+            isTestArea={true}></Input>
+          <Input type="date" ref={dueDate} label="Due Date"></Input>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
