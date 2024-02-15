8663ce707ccceced97814ce14594cc5cd777c692
fanzejie <fanzejie@cslc.com.cn>
2024-02-15 11:35:44 +0800

    在这一步使用了ref，forwardRef来做save按钮，让父组件newProject组件能读到Input组件中输入框的值,然后再状态提升到app组件中改变projects的值

--- 09PracticeProject-ProjectManagement/src/App.jsx
+++ 09PracticeProject-ProjectManagement/src/App.jsx
@@ -3,29 +3,42 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 function App() {
-  const [projectsState,setProjectsState] = useState({
+  const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects:[]
-  }) 
+    projects: [],
+  });
   function handleStartAddProject() {
-    setProjectsState(prevState =>{
+    setProjectsState((prevState) => {
       return {
         ...prevState,
-        selectedProjectId:null,
-        
-      }
+        selectedProjectId: null,
+      };
     });
   }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  console.log(projectsState);
   let content;
-  if(projectsState.selectedProjectId === null){
-    content = <NewProject/>
-  }else if(projectsState.selectedProjectId === undefined){
-    content =  <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
-     {content}
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- 09PracticeProject-ProjectManagement/src/components/NewProject.jsx
+++ 09PracticeProject-ProjectManagement/src/components/NewProject.jsx
@@ -1,14 +1,43 @@
-import Input from "./Input"
-export default function NewProject() {
-    return <div className="w-[35rem]">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="px-6 py-6 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
-        </menu>
-        <div>
-            <Input label="Title"></Input>
-            <Input label="Description" isTestArea={true}></Input>
-            <Input label="Due Date"></Input>
-        </div>
+import { useRef } from "react";
+import Input from "./Input";
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    //然后这里要把这些数据传到app.jsx中管理，要用到状态提升
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+  return (
+    <div className="w-[35rem]">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-6 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}>
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" ref={title} label="Title"></Input>
+        <Input ref={description} label="Description" isTestArea={true}></Input>
+        <Input type="date" ref={dueDate} label="Due Date"></Input>
+      </div>
     </div>
+  );
 }
