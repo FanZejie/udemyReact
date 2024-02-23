00d2435198e8f4ea15f2ef6f1d4d977b5ec60149
fanzejie <fanzejie@cslc.com.cn>
2024-02-23 15:46:34 +0800

    too much ，maybe i need to fix my git

--- 19-ReduxBasic/src/tmp.js
+++ 19-ReduxBasic/src/tmp.js
@@ -1,16 +1,45 @@
-var strStr = function(haystack, needle) {
-    let result = -1
-    for(let i=0;i<haystack.length;i++){
-        if(haystack[i] === needle[0]){
-             let tmpStr = haystack.slice(i,i+needle.length)
-             console.log(tmpStr)
-            if(tmpStr.valueOf() === needle.valueOf()){
-                result = i
-                return result
-            }
+// var removeDuplicates = function(nums) {
+//     let left = 0
+//     for(let i=1;i<nums.length;){
+//         if(nums[left] == nums[i]){
+//             nums.splice(i,1)
+
+//         }else{
+//             left++
+//             i++
+//         }
+//     }
+//     console.log(nums)
+//     return nums
+// };
+
+// var removeDuplicates = function(nums) {
+//     let left = 0
+//     for(let i=1;i<nums.length;){
+//         if(nums[left] == nums[i]){
+//             i++     
+//         }else{
+//             nums[left + 1] = nums[i]
+//             left++
+//             i++
+//         }
+//     }
+//     console.log(left)
+//     return nums
+// };
+//removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+
+
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g,"").replace('/\s/g',"").toLowerCase()
+    console.log(s.length)
+    
+    for(let i=0;i<s.length/2;i++){
+        if(s[i]!=s[s.length-1-i]){
+            return false
         }
     }
-    return result
+    return true
 };
 
-strStr("sadbutsad","sad")
+isPalindrome("A man, a plan, a canal: Panama")
